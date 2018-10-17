33579a321cd45b6be65b30263bd75f19592d15bc
Sune <fengel@gmail.com>
2018-10-17 11:18:45 +0200

    Updated all

--- src/helpers.js
+++ src/helpers.js
@@ -3,12 +3,13 @@ import {breakpoints as bp} from './settings.json'
 export const breakpoints = bp
 
 export const matchMedia = {
-  isSmall: window.matchMedia(`screen and (max-width: ${ breakpoints.small }px)`).matches,
+  isXSmall: window.matchMedia(`screen and (max-width: ${ breakpoints.xSmall }px)`).matches,
+  isSmall: window.matchMedia(`screen and (max-width: ${ breakpoints.medium - 1 }px)`).matches,
   isMediumUp: window.matchMedia(`screen and (min-width: ${ breakpoints.medium }px)`).matches,
   isMediumOnly: window.matchMedia(`screen and (min-width: ${ breakpoints.medium }px) and (max-width: ${ breakpoints.large - 1 }px)`).matches,
   isLargeUp: window.matchMedia(`screen and (min-width: ${ breakpoints.large }px)`).matches,
   isLargeOnly: window.matchMedia(`screen and (min-width: ${ breakpoints.large }px) and (max-width: ${ breakpoints.xLarge - 1 }px)`).matches,
   isXLargeUp: window.matchMedia(`screen and (min-width: ${ breakpoints.xLarge }px)`).matches,
-  isXXLargeOnly: window.matchMedia(`screen and (min-width: ${ breakpoints.xLarge }px) and (max-width: ${ breakpoints.xxLarge - 1 }px)`).matches,
+  isXLargeOnly: window.matchMedia(`screen and (min-width: ${ breakpoints.xLarge }px) and (max-width: ${ breakpoints.xxLarge - 1 }px)`).matches,
   isXXLargeUp: window.matchMedia(`screen and (min-width: ${ breakpoints.xxLarge }px)`).matches,
 }
